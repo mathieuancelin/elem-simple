4281c56bd614babd68475d434f166c56501969c8
Mathieu <mathieu.ancelin@gmail.com>
2015-12-18 07:08:38 +0100

    Add a test for special values

--- tests/children.spec.js
+++ tests/children.spec.js
@@ -32,4 +32,50 @@ describe('elem-simple : children', () => {
     expect(children[2].className).to.be.equal('item');
     app.cleanup();
   });
+  it('should handle components with null, undefined, false, and array children', () => {
+    const Item = (props) => <li className="item">{props.value}</li>;
+    const Wrapper = (props) => <ul className="wrapper">{props.children}</ul>;
+    const returnArray = () => [];
+    const returnBoolean = () => false;
+    const returnNull = () => null;
+    const returnUndefined = () => undefined;
+    const App = (props) => {
+      return (
+        <Wrapper>
+          <Item value="Item 1" />
+          {returnArray()}
+          <Item value="Item 2" />
+          {returnBoolean()}
+          <Item value="Item 3" />
+          {returnNull()}
+          <Item value="Item 4" />
+          {returnUndefined()}
+          <Item value="Item 5" />
+        </Wrapper>
+      );
+    };
+    const app = React.render(App, document.getElementById('app'));
+    const ul = document.querySelector('ul');
+    const children = ul.childNodes;
+    expect(ul.className).to.be.equal('wrapper');
+    expect(children.length).to.be.equal(5);
+    expect(children[0].innerHTML).to.be.equal('Item 1');
+    expect(children[0].tagName).to.be.equal('LI');
+    expect(children[0].className).to.be.equal('item');
+    expect(children[1].innerHTML).to.be.equal('Item 2');
+    expect(children[1].tagName).to.be.equal('LI');
+    expect(children[1].className).to.be.equal('item');
+    expect(children[2].innerHTML).to.be.equal('Item 3');
+    expect(children[2].tagName).to.be.equal('LI');
+    expect(children[2].className).to.be.equal('item');
+
+    expect(children[3].innerHTML).to.be.equal('Item 4');
+    expect(children[3].tagName).to.be.equal('LI');
+    expect(children[3].className).to.be.equal('item');
+
+    expect(children[4].innerHTML).to.be.equal('Item 5');
+    expect(children[4].tagName).to.be.equal('LI');
+    expect(children[4].className).to.be.equal('item');
+    app.cleanup();
+  });
 });

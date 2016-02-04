87e31b11030964b8f1fbcd9ded8374d18ca32fc4
Mathieu <mathieu.ancelin@gmail.com>
2016-02-04 15:37:21 +0100

    Add compat file

---
+++ tools/compat.js
@@ -0,0 +1,18 @@
+import * as React from '../src/index';
+import { isFunction } from '../src/utils';
+
+export * from '../src/index';
+
+// aliases to stay compatible with other version of Elem
+export const jsx = React.createElement;
+
+/**
+ * a simple function that return `what` if the predicate `p` is true
+ */
+export function predicate(p, what) {
+  if (isFunction(p)) { // if it's a function, call it
+    return !!p() ? what : undefined;
+  } else {
+    return !!p ? what : undefined;
+  }
+}
